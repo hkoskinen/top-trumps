3541a11e763f34c64c76f5c6a1282bdf73e12ee0
Henri Koskinen <hkoskinen@protonmail.com>
2018-06-24 21:03:50 +0300

    Update card component to have a selected stat style

--- src/card.js
+++ src/card.js
@@ -1,19 +1,19 @@
-const generateMarkupFromData = data => {
+const generateMarkupFromData = (data, selectedStat) => {
   return Object.entries(data).map(([key, val]) => {
     if (key === 'name' || key === 'version') return; // don't add name and version again
 
     const splittedKey = key.split(/(?=[A-Z])/).join(' ').toLowerCase();
     const prettyKey = splittedKey.substring(0, 1).toUpperCase() + splittedKey.substring(1)
     return `
-      <li onclick="app.selectStat('${key}')">${prettyKey}<span>${val}</span></li>
+      <li onclick="app.selectStat('${key}')" class="${key === selectedStat ? 'selected' : ''}">${prettyKey}<span>${val}</span></li>
     `;
   }).join('');
 };
 
-const card = (data, isPlayer = true) => {
+const card = (data, isPlayer = true, selectedStat) => {
   if (!data) return '<div class="card">Card data is missing</div>';
 
-  const stats = generateMarkupFromData(data);
+  const stats = generateMarkupFromData(data, selectedStat);
 
   return `
     <div class="card">
